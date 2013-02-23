d12eaab0ea70b8c6d697943fa1d432aa8ab9e0cb
pc <pwchurch@gmail.com>
2013-02-23 01:03:47 -0500

    Added template file

---
+++ ch1/template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            // template for future work
+        
+            // error reporting
+            ini_set('display_errors', 1);
+            error_reporting(E_ALL | E_STRICT);
+            
+            // more PHP code here
+        ?>
+    </body>
+</html>
