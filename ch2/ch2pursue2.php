dc8a2dd26227c6e9e4c483a9550d93ea8f40c1da
pc <pwchurch@gmail.com>
2013-02-23 01:18:47 -0500

    slight fix to ch2 exercise

--- ch2/ch2pursue2.php
+++ ch2/ch2pursue2.php
@@ -1,5 +1,5 @@
 <?php 
-    $fun_img = 'img/grumpycat.jpg';
+    $fun_img = 'http://i1.kym-cdn.com/photos/images/newsfeed/000/480/886/dee.jpg';
     $link_address = 'http://www.theuselessweb.com';
 ?>
 <!DOCTYPE html>
@@ -9,6 +9,7 @@
         <title></title>
     </head>
     <body>
+        <h1>Below is an image linking to internet hilarity created by PHP</h1>
         <a href="<?php echo $link_address; ?>">
             <img src="<?php echo $fun_img; ?>" style="margin:0 auto;height:auto;width:auto;border:none;padding:10px;" />
         </a>
