49b65844dc731ffb35fa90d864cf7f332b4dd0b0
pc <pwchurch@gmail.com>
2013-04-15 02:23:21 -0400

    Added chapter 8

---
+++ ch8/Pursue8-1index.php
@@ -0,0 +1,35 @@
+<?php
+    // Pursue 8-1 index.php
+    
+    /* This is the home page for this site
+     * It uses templates to create the layout
+    */
+     
+    // error handling
+    ini_set('error_reporting', 1);
+    error_reporting(E_STRICT | E_ALL);
+     
+    include('templates/Pursue8-1header.html');
+
+?>
+                <h2>Welcome to a J.D. Salinger Fan Club</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <h2>Another header</h2>
+                <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+                culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+<?php
+
+    include('templates/Pursue8-1footer.html');
+?>
---
+++ ch8/books.php
@@ -0,0 +1,17 @@
+<?php
+// Script 8.5 - books.php
+define('TITLE', 'Books by J.D. Salinger');
+
+include('templates/header.html');
+?>
+
+            <h2>J.D. Salinger's Books</h2>
+            <ul>
+                <li>The Catcher in the Rye</li>
+                <li>Nine Stories</li>
+                <li>Franny and Zooey</li>
+                <li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+            </ul>
+
+
+<?php include('templates/footer.html'); ?>
